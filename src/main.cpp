490cd620f8bd27f55e5abca7f4c876e051cbf2f3
mattys1 <mattys0082@gmail.com>
2024-12-22 13:46:09 +0100

    multiple threads and value modififying test

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
 #include <thread>
 #include <print>
+#include <vector>
 
-double getPi(const long deltax, const int threads) {
-	auto calculate = [&]() {
+double getPi(const double deltax, const int threads) {
+	double test = 0.;
+
+	auto calculate = [&]() -> double {
 		std::println("Hello World");
+		return test++;
 	};
 
-	std::jthread calculateThread(calculate);
+	std::vector<std::thread> threadPool;
+
+	for(int i = 0; i < threads; i++) {
+		threadPool.push_back(std::thread(calculate));
+	}
+
+	for(auto &thread : threadPool) {
+		thread.join();
+	}
+
+	std::println("Test: {}", test);
 
 	return 0.;
 }
 
 int main(int argc, char *argv[]) {
-	getPi(1, 1);
+	getPi(1, 10);
 }
