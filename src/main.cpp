29aafcaf5e1ce7443c5768ee13dcad335bc07e68
mattys1 <mattys0082@gmail.com>
2024-12-22 13:20:14 +0100

    thread test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
-int main(int argc, char *argv[]) {
+#include <thread>
+#include <print>
 
+double getPi(const long deltax, const int threads) {
+	auto calculate = [&]() {
+		std::println("Hello World");
+	};
+
+	std::jthread calculateThread(calculate);
+
+	return 0.;
+}
+
+int main(int argc, char *argv[]) {
+	getPi(1, 1);
 }
